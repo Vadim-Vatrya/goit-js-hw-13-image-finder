bd4eb3ea380ab1e552ad4dba816d7700db61ded0
Vadim-Vatrya <VatryaVV@gmail.com>
2021-01-01 23:57:29 +0200

    Fetch

--- src/index.js
+++ src/index.js
@@ -1,1 +1,5 @@
-import './styles.css';
+fetch(
+  'https://pixabay.com/api/?image_type=photo&orientation=horizontal&per_page=12&key=19735813-38939a283ca61b34fac8c005d',
+)
+  .then(res => res.json())
+  .then(console.log);
