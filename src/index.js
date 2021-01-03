8ababa54ffa0dceb2e76b107af3c06f823aceda3
Vadim-Vatrya <VatryaVV@gmail.com>
2021-01-03 12:49:17 +0200

    шаблон для  разметки

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
-fetch(
-  'https://pixabay.com/api/?image_type=photo&orientation=horizontal&per_page=12&key=19735813-38939a283ca61b34fac8c005d',
-)
+const apiKey = '19735813-38939a283ca61b34fac8c005d';
+const url =
+  ' https://pixabay.com/api/?image_type=photo&orientation=horizontal&order=latest&per_page=12&key=19735813-38939a283ca61b34fac8c005d';
+
+fetch(url)
   .then(res => res.json())
-  .then(console.log);
+  .then(data => console.log(data.hits))
+  .catch(error => console.log(error));
