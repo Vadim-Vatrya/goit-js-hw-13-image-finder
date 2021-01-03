42f7587f2cbf93638103cab4b26db35e6a0ae50e
Vadim-Vatrya <VatryaVV@gmail.com>
2021-01-03 13:08:18 +0200

    пофиксил шаблон

--- src/index.js
+++ src/index.js
@@ -1,8 +1,15 @@
+import galleryList from './templates/galleryList.hbs';
+
 const apiKey = '19735813-38939a283ca61b34fac8c005d';
 const url =
   ' https://pixabay.com/api/?image_type=photo&orientation=horizontal&order=latest&per_page=12&key=19735813-38939a283ca61b34fac8c005d';
 
 fetch(url)
   .then(res => res.json())
-  .then(data => console.log(data.hits))
+  .then(({ hits }) => {
+    console.log(hits);
+
+    markup = galleryList(hits);
+    console.log(markup);
+  })
   .catch(error => console.log(error));
